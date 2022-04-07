9837635239d7060aca876f1a2432a84362aedce7
ytmatos8807 <ytmatos8807@gmail.com>
2022-04-07 19:41:41 -0400

    Include class posts
    Include class posts

---
+++ Blog.Engine.Base/Blog.Engine.Base/Posts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Engine.Base
+{
+    [DataContract]
+    public class Posts
+    {
+        /// <summary>
+        /// Id de la publicacion
+        /// </summary>
+        [DataMember]
+        public Int32 PostsId { get; set; }
+
+        /// <summary>
+        /// Estado de la publicacion
+        /// </summary>
+        [DataMember]
+        public string Status { get; set; }
+
+        /// <summary>
+        ///Lista de  Id de los comentarios
+        /// </summary>
+        [DataMember]
+        public List<Int32> CommentId { get; set; }
+
+        /// <summary>
+        ///Cantidad de comentarios de la publicacion
+        /// </summary>
+        [DataMember]
+        public Int32 CountComment { get; set; }
+
+        /// <summary>
+        ///Titulo de la publicacion
+        /// </summary>
+        [DataMember]
+        public string Title { get; set; }
+
+        /// <summary>
+        ///Contenido de la publicacion
+        /// </summary>
+        [DataMember]
+        public string Content { get; set; }
+
+        /// <summary>
+        ///Autor de la publicacion
+        /// </summary>
+        [DataMember]
+        public string Author { get; set; }
+
+        /// <summary>
+        ///Fecha de la publicacion
+        /// </summary>
+        [DataMember]
+        public DateTime DatePosts { get; set; }
+
+    }
+}
